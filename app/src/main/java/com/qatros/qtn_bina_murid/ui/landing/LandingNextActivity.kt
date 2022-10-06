4ef0b812fb0bfae68bb911bd1a21434b1fe51004
satriomuslim <satriomuslim2627@gmail.com>
2022-10-06 17:02:56 +0700

    [satrio] layout landing

---
+++ app/src/main/java/com/qatros/qtn_bina_murid/ui/landing/LandingNextActivity.kt
@@ -0,0 +1,12 @@
+package com.qatros.qtn_bina_murid.ui.landing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qatros.qtn_bina_murid.R
+
+class LandingNextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_next)
+    }
+}
