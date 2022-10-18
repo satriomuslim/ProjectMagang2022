75fa8302c9b1f76beb7f03e73c796111d06f438d
Enrico Maricar <enrico.maricar@gmail.com>
2022-10-18 13:15:21 +0700

    [enrico]reset password

--- app/src/main/java/com/qatros/qtn_bina_murid/ui/resetPassword/CheckEmailFragment.kt
+++ app/src/main/java/com/qatros/qtn_bina_murid/ui/resetPassword/CheckEmailFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.qatros.qtn_bina_murid.R
+import com.qatros.qtn_bina_murid.databinding.FragmentCheckEmailBinding
 
 class CheckEmailFragment : Fragment() {
+    private lateinit var binding: FragmentCheckEmailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check_email, container, false)
+        binding = FragmentCheckEmailBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        with(binding) {
+            btnSendEmail.setOnClickListener {
+                findNavController().navigate(R.id.action_checkEmailFragment_to_setNewPasswordFragment)
+            }
+        }
     }
 }
