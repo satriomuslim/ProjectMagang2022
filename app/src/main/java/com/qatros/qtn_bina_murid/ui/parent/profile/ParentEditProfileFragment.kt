4eb857b86e71f2f454043b9c9c0fcd0b82906f78
riyanda2410 <riyandaas2410@gmail.com>
2022-10-28 16:27:48 +0700

    [Riyanda]Add Layout Pop Up Scan Barcode

--- app/src/main/java/com/qatros/qtn_bina_murid/ui/parent/profile/ParentEditProfileFragment.kt
+++ app/src/main/java/com/qatros/qtn_bina_murid/ui/parent/profile/ParentEditProfileFragment.kt
@@ -1,19 +1,78 @@
 package com.qatros.qtn_bina_murid.ui.parent.profile
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.qatros.qtn_bina_murid.R
+import com.qatros.qtn_bina_murid.databinding.FragmentFormChildBinding
+import com.qatros.qtn_bina_murid.databinding.FragmentParentEditProfileBinding
 
 class ParentEditProfileFragment : Fragment() {
+
+    private lateinit var binding : FragmentParentEditProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.apply{
+            edNamaLengkap.addTextChangedListener(loginTextWatcher)
+            edAlamat.addTextChangedListener(loginTextWatcher)
+            edNomorHandphone.addTextChangedListener(loginTextWatcher)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parent_edit_profile, container, false)
+        binding = FragmentParentEditProfileBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    private val loginTextWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            binding.apply{
+                when {
+                    edNamaLengkap.text!!.isEmpty() -> {
+                        edNamaLengkap.error = "Name Required"
+                    }
+                    edAlamat.text!!.isEmpty() -> {
+                        edAlamat.error = "Address Required"
+                    }
+                    edNomorHandphone.text!!.isEmpty() -> {
+                        edNomorHandphone.error = "Number Phone Required"
+                    }
+                    else -> {
+
+                    }
+
+                }
+                btnSaveDataProfileParent.isEnabled =  edNamaLengkap.text!!.isNotEmpty() && edAlamat.text!!.isNotEmpty()  && edNomorHandphone.text!!.isNotEmpty()
+
+            }
+
+        }
+
+        override fun afterTextChanged(s: Editable) {
+            binding.apply {
+                if (edNamaLengkap.text?.isBlank()?.not() == true && edAlamat.text?.isBlank()?.not() == true  && edNomorHandphone.text?.isBlank()?.not() == true) {
+                    btnSaveDataProfileParent.setBackgroundColor(resources.getColor(R.color.blue))
+                } else {
+                    btnSaveDataProfileParent.setBackgroundColor(resources.getColor(R.color.grey))
+                }
+            }
+        }
+
+    }
+
 }
